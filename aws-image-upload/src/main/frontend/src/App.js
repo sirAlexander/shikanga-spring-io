c46d65e2fd38d90c9a96f3d73dd2848e6875faca
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-06-06 22:26:44 +0100

    react dropzone

--- aws-image-upload/src/main/frontend/src/App.js
+++ aws-image-upload/src/main/frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import {useDropzone} from 'react-dropzone'
 import axios from "axios";
 import './App.css';
 
@@ -21,12 +22,36 @@ const UserProfiles = () => {
 
     return (
       <div key={index}>
+        {/* todo: profile image */}
+        <br/>
+        <br/>
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
+        <Dropzone />
+        <br />
       </div>
     )
   })
 };
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p>Drag 'n' drop profile image, or click to select profile image</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
