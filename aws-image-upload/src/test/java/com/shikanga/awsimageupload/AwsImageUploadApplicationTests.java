5d14ef3d18722af73cab00e2cbf7ceed17a0d300
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-06-06 15:36:33 +0100

    initial project commit

---
+++ aws-image-upload/src/test/java/com/shikanga/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shikanga.awsimageupload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsImageUploadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
