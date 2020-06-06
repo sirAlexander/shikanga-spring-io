b10f99e3eaa98f2ae543c422567d99574b348c61
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-06-06 23:02:10 +0100

    UI logic to send uploaded files to the backend

--- aws-image-upload/src/main/frontend/src/App.js
+++ aws-image-upload/src/main/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import {useDropzone} from 'react-dropzone'
+import { useDropzone } from 'react-dropzone'
 import axios from "axios";
 import './App.css';
 
@@ -23,23 +23,41 @@ const UserProfiles = () => {
     return (
       <div key={index}>
         {/* todo: profile image */}
-        <br/>
-        <br/>
+        <br />
+        <br />
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
-        <Dropzone />
+        <Dropzone {...userProfile} />
         <br />
       </div>
     )
   })
 };
 
-function Dropzone() {
+function Dropzone({ userProfileId }) {
   const onDrop = useCallback(acceptedFiles => {
     const file = acceptedFiles[0];
+
     console.log(file);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios.post(
+      `http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+      formData,
+      {
+        headers: {
+          "content-Type": "multipart/form-data"
+        }
+      }
+    ).then(() => {
+      console.log("file uploaded successfully")
+    }).catch(err => {
+      console.log(err);
+    });
   }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   return (
     <div {...getRootProps()}>
