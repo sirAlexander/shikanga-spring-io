5397569a9ab5ba9e492398792ecb985de0767f01
sirAlexander <alexander.shikanga.tindi@gmail.com>
2021-04-15 22:01:44 +0100

    Initialize project and start server

---
+++ getting-started-spring-boot/src/main/java/com/shikanga/GettingStartedSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.shikanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedSpringBootApplication.class, args);
+	}
+
+}
---
+++ getting-started-spring-boot/src/test/java/com/shikanga/GettingStartedSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
