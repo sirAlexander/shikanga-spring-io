687ed5af4550dd681edefd80e1b01bd2bf9bcf3d
sirAlexander <alexander.shikanga.tindi@gmail.com>
2021-07-21 20:39:06 +0100

    initial KnoteController

---
+++ knote-java/src/main/java/learnk8s/io/knotejava/KnoteController.java
@@ -0,0 +1,58 @@
+package learnk8s.io.knotejava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+public class KnoteController {
+
+    private final NotesRepository notesRepository;
+
+    public KnoteController(NotesRepository notesRepository) {
+        this.notesRepository = notesRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        getAllNotes(model);
+        return "index";
+    }
+
+    @PostMapping("/note")
+    public String saveNotes(@RequestParam("image") MultipartFile file,
+                            @RequestParam String description,
+                            @RequestParam(required = false) String publish,
+                            @RequestParam(required = false) String upload,
+                            Model model) throws IOException {
+
+        if (publish != null && publish.equals("Publish")) {
+            saveNote(description, model);
+            getAllNotes(model);
+            return "redirect:/";
+        }
+        // After save fetch all notes again
+        return "index";
+    }
+
+    private void getAllNotes(Model model) {
+        List<Note> notes = notesRepository.findAll();
+        Collections.reverse(notes);
+        model.addAttribute("notes", notes);
+    }
+
+    private void saveNote(String description, Model model) {
+        if (description != null && !description.trim().isEmpty()) {
+            notesRepository.save(new Note(null, description.trim()));
+            //After publish you need to clean up the textarea
+            model.addAttribute("description", "");
+        }
+    }
+}
