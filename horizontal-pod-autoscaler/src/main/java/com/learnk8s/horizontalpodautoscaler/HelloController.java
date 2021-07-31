6533b0b45d91bf4b8c67aa19655f559fd8f19b10
sirAlexander <alexander.shikanga.tindi@gmail.com>
2021-07-31 15:48:15 +0100

    updated controller to use model and values from property file

--- horizontal-pod-autoscaler/src/main/java/com/learnk8s/horizontalpodautoscaler/HelloController.java
+++ horizontal-pod-autoscaler/src/main/java/com/learnk8s/horizontalpodautoscaler/HelloController.java
@@ -1,5 +1,6 @@
 package com.learnk8s.horizontalpodautoscaler;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+    @Value("${queue.name}")
+    private String queueName;
+
+    @Value("${worker.name}")
+    private String workerName;
+
+    @Value("${store.enabled}")
+    private boolean storeEnabled;
+
+    @Value("${worker.enabled}")
+    private boolean workerEnabled;
+
     @GetMapping("/")
     public String home(Model model){
+        model.addAttribute("ticket", new Ticket());
+        model.addAttribute("queueName", this.queueName);
+        model.addAttribute("workerName", this.workerName);
+        model.addAttribute("isStoreEnabled", this.storeEnabled);
+        model.addAttribute("isWorkerEnabled", this.workerEnabled);
         return "home";
     }
 }
