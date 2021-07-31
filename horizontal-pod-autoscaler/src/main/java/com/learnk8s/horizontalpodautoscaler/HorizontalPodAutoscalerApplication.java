abb67f168f271f64539009eb4124419ecb912fd1
sirAlexander <alexander.shikanga.tindi@gmail.com>
2021-07-31 16:57:14 +0100

    wired springboot application to send and receive messages

--- horizontal-pod-autoscaler/src/main/java/com/learnk8s/horizontalpodautoscaler/HorizontalPodAutoscalerApplication.java
+++ horizontal-pod-autoscaler/src/main/java/com/learnk8s/horizontalpodautoscaler/HorizontalPodAutoscalerApplication.java
@@ -1,13 +1,34 @@
 package com.learnk8s.horizontalpodautoscaler;
 
+import com.learnk8s.horizontalpodautoscaler.queue.QueueService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListenerConfigurer;
+import org.springframework.jms.config.JmsListenerEndpointRegistrar;
+import org.springframework.jms.config.SimpleJmsListenerEndpoint;
 
 @SpringBootApplication
-public class HorizontalPodAutoscalerApplication {
+@EnableJms
+public class HorizontalPodAutoscalerApplication implements JmsListenerConfigurer {
+
+    private final QueueService queueService;
+
+    public HorizontalPodAutoscalerApplication(QueueService queueService) {
+        this.queueService = queueService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HorizontalPodAutoscalerApplication.class, args);
     }
 
+    @Override
+    public void configureJmsListeners(JmsListenerEndpointRegistrar jmsListenerEndpointRegistrar) {
+        SimpleJmsListenerEndpoint endpoint = new SimpleJmsListenerEndpoint();
+        endpoint.setId("myId");
+        endpoint.setDestination("queueName");
+        endpoint.setMessageListener(queueService);
+        jmsListenerEndpointRegistrar.registerEndpoint(endpoint);
+
+    }
 }
