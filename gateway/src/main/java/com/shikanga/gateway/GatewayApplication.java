aa8ad0e57afd53d35c56f0a83ab033e95b39b4a0
sir'Alexander <alexander@shikanga.co.uk>
2021-12-02 12:55:41 +0000

    added fallback functionality

--- gateway/src/main/java/com/shikanga/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/shikanga/gateway/GatewayApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class GatewayApplication {
 
 	private static final String HTTP_BIN_BASE_URL = "http://httpbin.org:80";
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
@@ -25,11 +29,17 @@ public class GatewayApplication {
 						.uri(HTTP_BIN_BASE_URL))
 				.route(p -> p
 						.host("*.circuitbreaker.com")
-						.filters(f -> f.circuitBreaker(
-								config -> config.setName("mycmd"))
-								)
+						.filters(f -> f
+								.circuitBreaker(config -> config
+										.setName("mycmd")
+										.setFallbackUri("forward:/fallback")))
 						.uri(HTTP_BIN_BASE_URL))
 				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
 	}
 
 }
