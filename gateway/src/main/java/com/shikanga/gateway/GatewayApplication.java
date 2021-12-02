ae20eb354125884fc599d9563e190b90cac02a49
sir'Alexander <alexander@shikanga.co.uk>
2021-12-02 13:19:49 +0000

    refactored to make use of UriConfiguration class

--- gateway/src/main/java/com/shikanga/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/shikanga/gateway/GatewayApplication.java
@@ -1,7 +1,11 @@
 package com.shikanga.gateway;
 
+import com.shikanga.gateway.GatewayApplication.UriConfiguration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,34 +16,48 @@ import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
+@EnableConfigurationProperties(UriConfiguration.class)
 public class GatewayApplication {
-
-	private static final String HTTP_BIN_BASE_URL = "http://httpbin.org:80";
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+		String httpUri = uriConfiguration.getHttpbin();
 		return builder.routes()
 				.route(p -> p
 						.path("/get")
 						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri(HTTP_BIN_BASE_URL))
+						.uri(httpUri))
 				.route(p -> p
 						.host("*.circuitbreaker.com")
 						.filters(f -> f
 								.circuitBreaker(config -> config
 										.setName("mycmd")
 										.setFallbackUri("forward:/fallback")))
-						.uri(HTTP_BIN_BASE_URL))
+						.uri(httpUri))
 				.build();
 	}
 
 	@RequestMapping("/fallback")
 	public Mono<String> fallback() {
 		return Mono.just("fallback");
+	}
+
+	@ConfigurationProperties
+	class UriConfiguration {
+
+		private String httpbin = "http://httpbin.org:80";
+
+		public String getHttpbin() {
+			return httpbin;
+		}
+
+		public void setHttpbin(String httpbin) {
+			this.httpbin = httpbin;
+		}
 	}
 
 }
