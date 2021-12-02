069ad1e1033a099f7f83c36ae89bdeece1045680
sir'Alexander <alexander@shikanga.co.uk>
2021-12-02 12:17:59 +0000

    added my first route to httpbin

--- gateway/src/main/java/com/shikanga/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/shikanga/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.shikanga.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,5 +13,14 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://httpbin.org:80"))
+				.build();
+	}
 
 }
