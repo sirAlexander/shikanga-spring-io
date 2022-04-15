55f929b6e60a4985b6d3bc449174a740cb16d295
sirAlexander <alexander.shikanga.tindi@gmail.com>
2022-04-15 10:51:13 +0100

    created user entities and repositories

---
+++ springboot-microservices/user-service/src/main/java/com/shikanga/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.shikanga.user.repository;
+
+import com.shikanga.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+}
