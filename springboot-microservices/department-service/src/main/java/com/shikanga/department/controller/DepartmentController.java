53ebecb9ff8e3941e85c369fc8bbede5bae09c4e
sirAlexander <alexander.shikanga.tindi@gmail.com>
2022-04-15 08:59:10 +0100

    added ability to save a department

--- springboot-microservices/department-service/src/main/java/com/shikanga/department/controller/DepartmentController.java
+++ springboot-microservices/department-service/src/main/java/com/shikanga/department/controller/DepartmentController.java
@@ -1,15 +1,27 @@
 package com.shikanga.department.controller;
 
+import com.shikanga.department.entity.Department;
 import com.shikanga.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
 
     private final DepartmentService departmentService;
 
     public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
+    }
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department){
+        log.info("Inside saveDepartment method of DepartmentController");
+        return departmentService.saveDepartment(department);
     }
 }
--- springboot-microservices/department-service/src/main/java/com/shikanga/department/service/DepartmentService.java
+++ springboot-microservices/department-service/src/main/java/com/shikanga/department/service/DepartmentService.java
@@ -1,14 +1,22 @@
 package com.shikanga.department.service;
 
+import com.shikanga.department.entity.Department;
 import com.shikanga.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
 
     public DepartmentService(DepartmentRepository departmentRepository) {
         this.departmentRepository = departmentRepository;
+    }
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment method of DepartmentService");
+        return departmentRepository.save(department);
     }
 }
