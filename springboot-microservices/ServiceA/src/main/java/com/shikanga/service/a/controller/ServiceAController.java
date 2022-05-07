8356d897848002a7117a7e8bb05192c49e0cd1dc
sirAlexander <alexander.shikanga.tindi@gmail.com>
2022-05-07 18:33:13 +0100

    added required circuit breaker dependencies

--- springboot-microservices/ServiceA/src/main/java/com/shikanga/service/a/controller/ServiceAController.java
+++ springboot-microservices/ServiceA/src/main/java/com/shikanga/service/a/controller/ServiceAController.java
@@ -1,6 +1,7 @@
 package com.shikanga.service.a.controller;
 
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,14 @@ public class ServiceAController {
     private final RestTemplate restTemplate;
 
     private static final String SERVICE_B_BASE_URL = "http://localhost:8182";
+    private static final String SERVICE_A = "service-A";
 
     public ServiceAController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @GetMapping
+    @CircuitBreaker(name = SERVICE_A)
     public String serviceA(){
         String url = SERVICE_B_BASE_URL + "/b";
         return restTemplate.getForObject(
