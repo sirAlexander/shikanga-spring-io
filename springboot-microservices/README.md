b6d0dd9a91d07b03f7bfcd9bc222e830d2074a2e
sirAlexander <alexander.shikanga.tindi@gmail.com>
2022-05-07 17:56:32 +0100

    created service-A for resilience4j exploration

--- springboot-microservices/README.md
+++ springboot-microservices/README.md
@@ -1,3 +1,4 @@
 # Microservices Implemented Using SpringBoot
 
 Implementing Microservice Architecture using the Spring Boot framework
+
---
+++ springboot-microservices/ServiceA/src/test/java/com/shikanga/service/a/ServiceAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikanga.service.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
