d15a2bdd06a78ca6d0707da5591d7d9df2dacaa8
sirAlexander <alexander.shikanga.tindi@gmail.com>
2022-05-07 17:58:51 +0100

    created service-B for resilience4j exploration

---
+++ springboot-microservices/ServiceB/src/test/java/com/shikanga/service/b/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikanga.service.b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
