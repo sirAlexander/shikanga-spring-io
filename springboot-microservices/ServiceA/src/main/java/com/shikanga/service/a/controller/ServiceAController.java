427cb4b928325333b6e48db56008fec62200176b
sirAlexander <alexander.shikanga.tindi@gmail.com>
2022-05-07 18:27:38 +0100

    initial implementation of API's

--- springboot-microservices/ServiceA/src/main/java/com/shikanga/service/a/controller/ServiceAController.java
+++ springboot-microservices/ServiceA/src/main/java/com/shikanga/service/a/controller/ServiceAController.java
@@ -1,8 +1,29 @@
 package com.shikanga.service.a.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/a")
 public class ServiceAController {
+
+    private final RestTemplate restTemplate;
+
+    private static final String SERVICE_B_BASE_URL = "http://localhost:8182";
+
+    public ServiceAController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping
+    public String serviceA(){
+        String url = SERVICE_B_BASE_URL + "/b";
+        return restTemplate.getForObject(
+                url,
+                String.class
+        );
+    }
 }
--- springboot-microservices/ServiceB/src/main/java/com/shikanga/service/b/controller/ServiceBController.java
+++ springboot-microservices/ServiceB/src/main/java/com/shikanga/service/b/controller/ServiceBController.java
@@ -1,8 +1,16 @@
 package com.shikanga.service.b.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/b")
 public class ServiceBController {
+
+    @GetMapping
+    public String serviceB(){
+        return "Service B has been called";
+    }
 }
